cfb74c432e24b7f6d0a0b3733c3df6af713cbdd8
Qubert <upafourth@gmail.com>
2017-11-29 20:49:42 +0100

    Add project files.

---
+++ CTCI 1.7 Set To Zero/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI_1._7_Set_To_Zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
